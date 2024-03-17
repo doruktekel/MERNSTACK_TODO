391992106f5b1f5810db79be9605bb244e05cfb0
doruk.tekel <doruk.tekel@bilgeadam.com>
2024-03-17 21:02:45 +0300

    done

--- frontend/src/components/Todos.jsx
+++ frontend/src/components/Todos.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Todo from "./Todo";
+import Swal from "sweetalert2";
+import Input from "./Input";
 
 const ToDos = () => {
   const [todos, setTodos] = useState([]);
@@ -11,48 +13,109 @@ const ToDos = () => {
     await axios
       .get("https://mernstack-todo-64u6.onrender.com/todos")
       .then((res) => {
-        setTodos(res.data);
         setLoading(false);
+        setTodos(res.data);
       })
       .catch((err) => console.log(err));
   };
 
-  const handleDeleteAllTodos = () => {};
-  const handleDeleteCompletedTodos = () => {};
+  const handleDeleteAllTodos = async () => {
+    const result = await Swal.fire({
+      title: "Are you sure to delete all todos ?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    });
+    if (result.isConfirmed) {
+      try {
+        await axios.delete("https://mernstack-todo-64u6.onrender.com/todos");
+        Swal.fire({
+          title: "Deleted!",
+          text: "Your file has been deleted.",
+          icon: "success",
+        });
+        await getTodos();
+      } catch (error) {
+        console.log("Error", error);
+      }
+    }
+  };
+
+  const handleDeleteCompletedTodos = async () => {
+    const result = await Swal.fire({
+      title: "Are you sure to delete completed todos ?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    });
+    if (result.isConfirmed) {
+      try {
+        const completedTodos = todos.filter((todo) => todo.completed === true);
+        await Promise.all(
+          completedTodos.map((item) =>
+            axios.delete(
+              `https://mernstack-todo-64u6.onrender.com/todos/${item._id}`
+            )
+          )
+        );
+        Swal.fire({
+          title: "Deleted!",
+          text: "Your file has been deleted.",
+          icon: "success",
+        });
+        await getTodos();
+      } catch (error) {
+        console.log("Error", error);
+      }
+    }
+  };
 
   useEffect(() => {
     getTodos();
   }, []);
 
-  useEffect(() => {
-    getTodos();
-  }, [todos]);
-
   return (
     <div>
-      <div className="flex flex-col gap-5">
-        {todos && todos.length > 0
-          ? todos.map((todoitem, index) => {
-              return (
-                <Todo key={index} todoitem={todoitem} getTodos={getTodos} />
-              );
-            })
-          : null}
-      </div>
-      <div className="flex justify-center align-middle gap-10 m-5">
-        <button
-          onClick={handleDeleteAllTodos}
-          className="px-4 py-2 bg-indigo-400 text-white rounded-lg cursor-pointer border-2 border-indigo-400 hover:shadow-xl hover:bg-transparent  transition ease-in-out delay-150  "
-        >
-          Delete All ToDos
-        </button>
-        <button
-          onClick={handleDeleteCompletedTodos}
-          className="px-4 py-2 bg-indigo-400 text-white rounded-lg cursor-pointer border-2 border-indigo-400 hover:shadow-xl hover:bg-transparent  transition ease-in-out delay-150  "
-        >
-          Delete Completed ToDos
-        </button>
-      </div>
+      <Input getTodos={getTodos} />
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div className="flex flex-col gap-5">
+          {todos && todos.length > 0
+            ? todos.map((todoitem) => {
+                return (
+                  <Todo
+                    key={todoitem._id}
+                    todoitem={todoitem}
+                    getTodos={getTodos}
+                  />
+                );
+              })
+            : null}
+        </div>
+      )}
+      {todos && todos.length > 1 ? (
+        <div className="flex justify-center align-middle gap-10 m-5">
+          <button
+            onClick={handleDeleteAllTodos}
+            className="px-4 py-2 bg-indigo-400 text-white rounded-lg cursor-pointer border-2 border-indigo-400 hover:shadow-xl hover:bg-transparent  transition ease-in-out delay-150  "
+          >
+            Delete All ToDos
+          </button>
+          <button
+            onClick={handleDeleteCompletedTodos}
+            className="px-4 py-2 bg-indigo-400 text-white rounded-lg cursor-pointer border-2 border-indigo-400 hover:shadow-xl hover:bg-transparent  transition ease-in-out delay-150  "
+          >
+            Delete Completed ToDos
+          </button>
+        </div>
+      ) : null}
     </div>
   );
 };
